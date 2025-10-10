1e5204420d4ddc0311095fb2f51dfb53502df830
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2025-10-10 13:13:57 +0200

    refactor: Renamed pellcorp to simplyprint

--- create.sh
+++ create.sh
@@ -36,22 +36,22 @@ echo "Firmware password: ${FIRMWARE_PASSWORD}"
 version="6.${CREALITY_VERSION}"
 
 function write_ota_info() {
-    echo "ota_version=${version}" > /tmp/${version}-pellcorp/ota_info
-    echo "ota_board_name=${board_name}" >> /tmp/${version}-pellcorp/ota_info
-    echo "ota_compile_time=$(date '+%Y %m.%d %H:%M:%S')" >> /tmp/${version}-pellcorp/ota_info
-    echo "ota_site=http://192.168.43.52/ota/board_test" >> /tmp/${version}-pellcorp/ota_info
-    sudo cp /tmp/${version}-pellcorp/ota_info /tmp/${version}-pellcorp/squashfs-root/etc/
+    echo "ota_version=${version}" > /tmp/${version}-simplyprint/ota_info
+    echo "ota_board_name=${board_name}" >> /tmp/${version}-simplyprint/ota_info
+    echo "ota_compile_time=$(date '+%Y %m.%d %H:%M:%S')" >> /tmp/${version}-simplyprint/ota_info
+    echo "ota_site=http://192.168.43.52/ota/board_test" >> /tmp/${version}-simplyprint/ota_info
+    sudo cp /tmp/${version}-simplyprint/ota_info /tmp/${version}-simplyprint/squashfs-root/etc/
 }
 
 function customise_rootfs() {
     write_ota_info
-    sudo cp $CURRENT_DIR/etc/init.d/* /tmp/${version}-pellcorp/squashfs-root/etc/init.d/
-    sudo sed -i "/^root/c\\$(printf '%s\n' "$ROOT_HASH")"  /tmp/${version}-pellcorp/squashfs-root/etc/shadow
-    sudo cp $CURRENT_DIR/root/* /tmp/${version}-pellcorp/squashfs-root/root/
+    sudo cp $CURRENT_DIR/etc/init.d/* /tmp/${version}-simplyprint/squashfs-root/etc/init.d/
+    sudo sed -i "/^root/c\\$(printf '%s\n' "$ROOT_HASH")"  /tmp/${version}-simplyprint/squashfs-root/etc/shadow
+    sudo cp $CURRENT_DIR/root/* /tmp/${version}-simplyprint/squashfs-root/root/
 }
 
 function update_rootfs() {
-    pushd /tmp/${version}-pellcorp/ > /dev/null
+    pushd /tmp/${version}-simplyprint/ > /dev/null
     sudo unsquashfs orig_rootfs.squashfs 
     customise_rootfs
     sudo mksquashfs squashfs-root rootfs.squashfs || exit $?
@@ -79,52 +79,52 @@ fi
 
 7z x /tmp/$old_image_name -p"$FIRMWARE_PASSWORD" -o/tmp
 
-if [ -d /tmp/${version}-pellcorp ]; then
-    sudo rm -rf /tmp/${version}-pellcorp
+if [ -d /tmp/${version}-simplyprint ]; then
+    sudo rm -rf /tmp/${version}-simplyprint
 fi
-mkdir -p /tmp/${version}-pellcorp/$directory/$sub_directory
+mkdir -p /tmp/${version}-simplyprint/$directory/$sub_directory
 
-cat /tmp/$old_directory/$old_sub_directory/rootfs.squashfs.* > /tmp/${version}-pellcorp/orig_rootfs.squashfs
-orig_rootfs_md5=$(md5sum /tmp/${version}-pellcorp/orig_rootfs.squashfs | awk '{print $1}')
-orig_rootfs_size=$(stat -c%s /tmp/${version}-pellcorp/orig_rootfs.squashfs)
+cat /tmp/$old_directory/$old_sub_directory/rootfs.squashfs.* > /tmp/${version}-simplyprint/orig_rootfs.squashfs
+orig_rootfs_md5=$(md5sum /tmp/${version}-simplyprint/orig_rootfs.squashfs | awk '{print $1}')
+orig_rootfs_size=$(stat -c%s /tmp/${version}-simplyprint/orig_rootfs.squashfs)
 
 # do the changes here
 update_rootfs || exit $?
 
-rootfs_md5=$(md5sum /tmp/${version}-pellcorp/rootfs.squashfs | awk '{print $1}')
-rootfs_size=$(stat -c%s /tmp/${version}-pellcorp/rootfs.squashfs)
+rootfs_md5=$(md5sum /tmp/${version}-simplyprint/rootfs.squashfs | awk '{print $1}')
+rootfs_size=$(stat -c%s /tmp/${version}-simplyprint/rootfs.squashfs)
 
-echo "current_version=$version" > /tmp/${version}-pellcorp/$directory/ota_config.in
-echo "" > /tmp/${version}-pellcorp/$directory/$sub_directory/ota_v${version}.ok
+echo "current_version=$version" > /tmp/${version}-simplyprint/$directory/ota_config.in
+echo "" > /tmp/${version}-simplyprint/$directory/$sub_directory/ota_v${version}.ok
 
-cp /tmp/$old_directory/$old_sub_directory/ota_update.in /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$old_directory/$old_sub_directory/ota_md5_xImage* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$old_directory/$old_sub_directory/ota_md5_zero.bin* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$old_directory/$old_sub_directory/zero.bin.* /tmp/${version}-pellcorp/$directory/$sub_directory/
-cp /tmp/$old_directory/$old_sub_directory/xImage.* /tmp/${version}-pellcorp/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_update.in /tmp/${version}-simplyprint/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_md5_xImage* /tmp/${version}-simplyprint/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/ota_md5_zero.bin* /tmp/${version}-simplyprint/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/zero.bin.* /tmp/${version}-simplyprint/$directory/$sub_directory/
+cp /tmp/$old_directory/$old_sub_directory/xImage.* /tmp/${version}-simplyprint/$directory/$sub_directory/
 
-pushd /tmp/${version}-pellcorp/$directory/$sub_directory > /dev/null
-split -d -b 1048576 -a 4 /tmp/${version}-pellcorp/rootfs.squashfs rootfs.squashfs.
+pushd /tmp/${version}-simplyprint/$directory/$sub_directory > /dev/null
+split -d -b 1048576 -a 4 /tmp/${version}-simplyprint/rootfs.squashfs rootfs.squashfs.
 popd > /dev/null
 
 part_md5=
-for i in $(ls /tmp/${version}-pellcorp/$directory/$sub_directory/rootfs.squashfs.*); do
+for i in $(ls /tmp/${version}-simplyprint/$directory/$sub_directory/rootfs.squashfs.*); do
     file=$(basename $i)
     if [ -z "$part_md5" ]; then
         id=$rootfs_md5
     else
         id=$part_md5
     fi
-    mv "/tmp/${version}-pellcorp/$directory/$sub_directory/$file" "/tmp/${version}-pellcorp/$directory/$sub_directory/${file}.${id}"
-    part_md5=$(md5sum /tmp/${version}-pellcorp/$directory/$sub_directory/${file}.${id} | awk '{print $1}')
-    echo "$part_md5" >> "/tmp/${version}-pellcorp/$directory/$sub_directory/ota_md5_rootfs.squashfs.${rootfs_md5}"
+    mv "/tmp/${version}-simplyprint/$directory/$sub_directory/$file" "/tmp/${version}-simplyprint/$directory/$sub_directory/${file}.${id}"
+    part_md5=$(md5sum /tmp/${version}-simplyprint/$directory/$sub_directory/${file}.${id} | awk '{print $1}')
+    echo "$part_md5" >> "/tmp/${version}-simplyprint/$directory/$sub_directory/ota_md5_rootfs.squashfs.${rootfs_md5}"
 done
 
-sed -i "s/ota_version=$CREALITY_VERSION/ota_version=$version/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
-sed -i "s/img_md5=$orig_rootfs_md5/img_md5=$rootfs_md5/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
-sed -i "s/img_size=$orig_rootfs_size/img_size=$rootfs_size/g" /tmp/${version}-pellcorp/$directory/$sub_directory/ota_update.in
+sed -i "s/ota_version=$CREALITY_VERSION/ota_version=$version/g" /tmp/${version}-simplyprint/$directory/$sub_directory/ota_update.in
+sed -i "s/img_md5=$orig_rootfs_md5/img_md5=$rootfs_md5/g" /tmp/${version}-simplyprint/$directory/$sub_directory/ota_update.in
+sed -i "s/img_size=$orig_rootfs_size/img_size=$rootfs_size/g" /tmp/${version}-simplyprint/$directory/$sub_directory/ota_update.in
 
-pushd /tmp/${version}-pellcorp/ > /dev/null
+pushd /tmp/${version}-simplyprint/ > /dev/null
 7z a ${image_name}.7z -p"$FIRMWARE_PASSWORD" $directory
 mv ${image_name}.7z "${CURRENT_DIR}/${image_name}"
 popd > /dev/null
